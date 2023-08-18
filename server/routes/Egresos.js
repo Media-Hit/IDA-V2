87d569cd590f0b93fec619d7dc72aece4270eb6a
MediaHit <info@mediahit-agency.com>
2023-08-17 20:10:04 -0500

    api bidireccional básica ok

--- server/routes/Egresos.js
+++ server/routes/Egresos.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { Egresos } = require("../models")
 
 router.get("/movimientos", (req, res) => {
     res.json("Hello World");
+});
+
+router.post("/nuevo-egreso", async (req, res) => {
+    const egreso = req.body;
+    await Egresos.create(egreso);
+    res.json(egreso);
 });
 
 
