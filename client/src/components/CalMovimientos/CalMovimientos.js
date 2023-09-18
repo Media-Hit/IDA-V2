1dae4d36a420fcdcb4bb374667d3dac5cb9f1e14
MediaHit <info@mediahit-agency.com>
2023-09-18 11:55:32 -0500

    datepicker guarda en state la fecha seleccionada

--- client/src/components/CalMovimientos/CalMovimientos.js
+++ client/src/components/CalMovimientos/CalMovimientos.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import React from "react";
+import { useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
-import './CalMovimientos.css';
-
+import "./CalMovimientos.css";
 
 function CalMovimientos() {
+  const [selectedDate, setSelectedDate] = useState([]);
 
   return (
-  <>
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DateCalendar showDaysOutsideCurrentMonth fixedWeekNumber={6} />
-    </LocalizationProvider>
-  </> 
-  )}
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DateCalendar
+          showDaysOutsideCurrentMonth
+          fixedWeekNumber={6}
+          onChange={(newValue) => {
+            setSelectedDate(newValue);
+            console.log(selectedDate);
+          }}
+        />
+      </LocalizationProvider>
+    </>
+  );
+}
 
 export { CalMovimientos };
