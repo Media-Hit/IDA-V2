73b5e1efd239bfee00beee890a01dfcd65015b48
MediaHit <info@mediahit-agency.com>
2023-09-28 18:46:13 -0500

    contexto funcionando en primera página

---
+++ client/src/pages/MovimientosPage/MovimientosContext/index.js
@@ -0,0 +1,43 @@
+import React, { createContext, useState } from "react";
+
+const MovimientosContext = createContext();
+
+function MovimientosProvider({ children }) {
+  //Página de Movimientos
+
+  const [selectedDate, setSelectedDate] = useState(null); // Estado compartido para la fecha seleccionada
+
+  const handleClearFilter = () => {
+    setSelectedDate(null);
+  };
+
+  const handleAddButtonClick = () => {
+    // Guardar la fecha seleccionada en localStorage
+    if (selectedDate) {
+      localStorage.setItem("selectedDate", selectedDate);
+    }
+  };
+
+  // Función para borrar selectedDate del localStorage
+  const clearLocalStorage = () => {
+    localStorage.removeItem("selectedDate");
+  };
+  clearLocalStorage();
+
+  return (
+    <MovimientosContext.Provider
+      value={{
+        selectedDate,
+        setSelectedDate,
+        handleClearFilter,
+        handleAddButtonClick,
+        handleAddButtonClick,
+        clearLocalStorage,
+      }}
+    >
+      {children}
+    </MovimientosContext.Provider>
+  );
+}
+
+export { MovimientosContext, MovimientosProvider };
